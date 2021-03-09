d326eefa053363df221b85c4312d66d426caaa70
Mali Scheuer <youremail@gmx.net>
2021-03-09 12:52:11 +0100

    implemted globalstyles

---
+++ src/GlobalStyles.js
@@ -0,0 +1,16 @@
+import { createGlobalStyle } from 'styled-components';
+
+export default createGlobalStyle`
+
+*{
+    box-sizing: border-box;
+   
+    font-family: 'Helvetica', 'Arial', sans-serif;
+}
+
+body{
+    margin: 0;
+ 
+}
+
+`
