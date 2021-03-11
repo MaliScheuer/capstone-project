b2fb9490c5af45f48f225d682b98ba4376d8244b
Mali Scheuer <youremail@gmx.net>
2021-03-11 09:11:38 +0100

    added css variables colors

--- src/GlobalStyles.js
+++ src/GlobalStyles.js
@@ -10,7 +10,19 @@ export default createGlobalStyle`
 
 body{
     margin: 0;
+    background: var(--red);
  
+}
+
+:root{
+
+    --red: #ff6750;
+    --petrol: #3d717d;
+    --petrol-light: #77b1b5;
+    --rose: #f2b29b;
+    --grey: #707070;
+    --lightgrey: #f0e6e6;
+    --kakhi: #7a8775;
 }
 
 `
