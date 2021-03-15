936ef13269237eb2523c1f86dab71606da4837e4
Mali Scheuer <youremail@gmx.net>
2021-03-15 16:21:15 +0100

    implemented server & get/post route for mentors

--- server/src/models/mentors.model.js
+++ server/src/models/mentors.model.js
@@ -1,15 +1,32 @@
 import mongoose from 'mongoose';
 
 const mentorsSchema = {
-    name: {
+    mentor_name: {
         type: String, required: true
     },
-    email:
-        { type: String, required: true }
+    competence: {
+        type: String, required: true
+    },
+    buzzwords: [{
+        type: String, required: true
+    }],
+    email: {
+        type: String, required: true
+    },
+    phone: {
+        type: String, required: true
+    },
+    about: {
+        type: String, required: true
+    },
+    image:
+        { type: String }
 };
 const Mentors = mongoose.model("mentor", mentorsSchema);
 
 
 
 export default Mentors;
+
+
 
--- server/src/server.js
+++ server/src/server.js
@@ -1,18 +1,43 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
-//import cors from 'cors';
+import Mentors from './models/mentors.model.js'
+import cors from 'cors';
 
 
 const connectionString = "mongodb://localhost:27017/mentor-app";
 
 const server = express();
 server.use(bodyParser.json());
-//server.use(cors());
-
-server.get('/', (req, res) => res.send('Hello'))
+server.use(cors());
 
 mongoose.connect(connectionString,
     { useNewUrlParser: true, useUnifiedTopology: true });
 
+server.get('/', (req, res) => res.send('Hello'))
+
+
+server.get('/search-mentors', (request, response) => {
+    Mentors.find().then(mentors => response.json(mentors))
+})
+
+server.post('/create-profile', (request, response) => {
+    const mentor = new Mentors({
+        mentor_name: request.body.mentor_name,
+        competence: request.body.competence,
+        buzzwords: request.body.buzzwords,
+        email: request.body.email,
+        phone: request.body.phone,
+        about: request.body.about,
+        image: request.body.image
+    })
+
+    mentor.save()
+        .then(mentor => response.json(mentor))
+        .catch(error => response.json(error))
+})
+
+
+
 server.listen(4000);
+
