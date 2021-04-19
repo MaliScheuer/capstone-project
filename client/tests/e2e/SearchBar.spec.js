d28c20c717400f93b65a19315a1c0f4f2d764a64
Mali Scheuer <youremail@gmx.net>
2021-04-19 16:42:42 +0200

    changes testing

--- client/tests/e2e/SearchBar.spec.js
+++ client/tests/e2e/SearchBar.spec.js
@@ -15,7 +15,7 @@ describe("SearchBar Component E2E tested", () => {
 
   it("it renders all mentors if the input field is empty and no competence field is choosen", () => {
     cy.get(SEARCH_INPUT_FIELD);
-    cy.get(MENTOR_CARD).should("have.length", 12);
+    cy.get(MENTOR_CARD).should("have.length", 13);
   });
 
   it("renders all mentors those skills are matching the search input field", () => {
@@ -23,12 +23,12 @@ describe("SearchBar Component E2E tested", () => {
     cy.get(MENTOR_CARD).should("have.length", 3);
   });
 
-  it("renders all mentors who matching the field of competence choosen in the dropdown", () => {
+  it("renders all mentors matching the field of competence choosen in the dropdown", () => {
     cy.get(DROPDOWN_COMPETENCE_FIELD).select("Arts and Entertainment");
     cy.get(MENTOR_CARD).should("have.length", 2);
   });
 
-  it("renders all mentors who matching the search query input OR field of competence dropdown", () => {
+  it("renders all mentors matching the search query input OR field of competence dropdown", () => {
     cy.get(DROPDOWN_COMPETENCE_FIELD).select("Health and Medicine");
     cy.get(SEARCH_INPUT_FIELD).type("self employment");
     cy.get(MENTOR_CARD).should("have.length", 4);
