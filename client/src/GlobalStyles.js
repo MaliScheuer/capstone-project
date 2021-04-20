3ec895532cc2cfa2603dbeff3db498a3c16c1ae3
Mali Scheuer <youremail@gmx.net>
2021-04-20 14:13:48 +0200

    edited global styles

--- client/src/GlobalStyles.js
+++ client/src/GlobalStyles.js
@@ -13,7 +13,7 @@ body{
     margin: 0;
     background: url("https://images.unsplash.com/photo-1547104442-044448b73426?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80");
     background-size: cover;
-    xbackground-repeat: no-repeat;
+   
     
 }
 
